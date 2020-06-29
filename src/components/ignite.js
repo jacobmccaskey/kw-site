57e20cd03db46d1256fab4f5b057d0b9a4390b0c
Jacob McCaskey <mccaskey316@gmail.com>
2020-06-29 09:45:22 -0400

    adds course videos

--- src/components/ignite.js
+++ src/components/ignite.js
@@ -16,7 +16,7 @@ export default function Ignite() {
           </span>
           <br />
           <iframe
-            title="smart plans"
+            title="Ignite 5"
             className="videos"
             src="https://www.youtube.com/embed/AP89bEWyqpw"
             frameBorder="0"
@@ -33,9 +33,41 @@ export default function Ignite() {
           </span>
           <br />
           <iframe
-            title="smart plans"
+            title="Ignite 6"
             className="videos"
             src="https://www.youtube.com/embed/G5khcQCH6hY"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="container mt-4 mb-4">
+          <hr />
+          <h2 className="coach-p">Ignite 7</h2>
+          <span>
+            <i>ignite course</i>
+          </span>
+          <br />
+          <iframe
+            title="Ignite 7"
+            className="videos"
+            src="https://www.youtube.com/embed/oOtiRS2ENmM"
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <div className="container mt-4 mb-4">
+          <hr />
+          <h2 className="coach-p">Ignite 8</h2>
+          <span>
+            <i>ignite course</i>
+          </span>
+          <br />
+          <iframe
+            title="Ignite 8"
+            className="videos"
+            src="https://www.youtube.com/embed/jlGgc80AlI8"
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
